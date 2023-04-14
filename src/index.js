fc6b1c080da2576f28a125a4922e9f01a966fda5
AWildTerryy <daniel.tarsoo@gmail.com>
2023-04-14 05:31:20 -0400

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
+
+
+function TrackButton(){
+  const [count, setCount] = useState(0);
+  
+  function handleClick(){
+    setCount(count + 1);
+  }
+  
+  return(
+    <button onClick={handleClick}>
+      Clicked {count} times!
+    </button>
+  )
+}
+
+function ButtonApp(){
+  return(
+    <div className="App">
+      <header className="App-header">
+        <h1>Chasing the Clicks</h1>
+        <TrackButton />
+      </header>  
+    </div>
+  )
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ButtonApp />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
